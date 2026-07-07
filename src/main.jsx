473384a156874ee6a02f31f34ddae139501ccb91
ArifinRamadhan <arifin23ti@mahasiswa.pcr.ac.id>
2026-07-07 15:23:45 +0700

    Modif main jsx buat arahkan ke Landing Page (Percobaan)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './assets/style.css'
+import Suarasa from './pages/Suarasa.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suarasa />
   </StrictMode>,
 )
